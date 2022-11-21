c1a54aa47819ec4737969f76e8db413327a7e0e0
Masoud Gharagozlu <gharagozlu.masoud@gmail.com>
2022-11-21 15:51:25 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php echo 'Hello';
+<?php
+$redis = new Redis();
+$redis->connect('redis', 6379);
+
+$allKeys = $redis->keys('*');
+
+$redis->set("key-".count($allKeys), date("H:i:s"));
+
+foreach($allKeys as $key){
+  echo $redis->get($key);
+}
